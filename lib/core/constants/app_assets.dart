2fea30782bf928c5047890a664b7d19f7e95be3c
Fazel <farissalman101010@gmail.com>
2026-05-16 13:37:16 +0700

    updateeeeee logooooooo

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,13 +1,13 @@
 final class AppAssets {
   const AppAssets._();
 
-  static const String iconsPath = 'lib/assets/icons';
-  static const String imagesPath = 'lib/assets/images';
+  static const String iconsPath = 'assets/icons';
+  static const String imagesPath = 'assets/images';
 
   static const String logoPeduliKeluarga =
       '$imagesPath/logo_peduli_keluarga.png';
 
-  static const String illustrationsPath = 'lib/assets/illustrations';
-  static const String animationsPath = 'lib/assets/animations';
-  static const String soundsPath = 'lib/assets/sounds';
+  static const String illustrationsPath = 'assets/illustrations';
+  static const String animationsPath = 'assets/animations';
+  static const String soundsPath = 'assets/sounds';
 }
